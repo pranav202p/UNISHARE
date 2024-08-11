aeb60684490d06b4b781f52614c9d3c1e83b8768
pranav202p <pranavpravi2002@gmail.com>
2024-08-11 11:51:55 +0530

    ==

--- UNISHARE/server/Script.js
+++ UNISHARE/server/Script.js
@@ -88,7 +88,9 @@ io.on('connection', (socket) => {
 
 // API routes
 app.use('/api/v1/auth', authRoutes);
-
+app.get("/",(req,res)=>{
+    res.json("Hello")
+})
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
     console.log(`Server Running on ${PORT}`);
